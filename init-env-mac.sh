d589d5f567f866b241be779dff6b9d9a4da5207d
Gabriel Jauffret <gabjauf@hotmail.fr>
2020-11-01 13:28:35 +0100

    Add fira code install

--- init-env-mac.sh
+++ init-env-mac.sh
@@ -5,3 +5,8 @@ curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
 
 # Install Oh-My-ZSH
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+# Install code font for powerline
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code-nerd-font
+
