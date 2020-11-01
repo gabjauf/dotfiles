0bc9ed483a50214170f17a8736ca8d8628e886c0
Gabriel Jauffret <gabjauf@hotmail.fr>
2020-11-01 13:18:38 +0100

    Add basic homebrew and oh-my-zsh install

---
+++ init-env-mac.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install brew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+
+# Install Oh-My-ZSH
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
